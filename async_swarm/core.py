644bed99f1c7813fedcfee2300372c09173a93bf
Adrian Carpenter <adriatic.c@gmail.com>
2025-02-18 04:00:28 -0800

    run_and_stream correctly iterates over chunks

--- async_swarm/core.py
+++ async_swarm/core.py
@@ -176,7 +176,7 @@ class AsyncSwarm:
             )
 
             yield {"delim": "start"}
-            for chunk in completion:
+            async for chunk in completion:
                 delta = json.loads(chunk.choices[0].delta.json())
                 if delta["role"] == "assistant":
                     delta["sender"] = active_agent.name
